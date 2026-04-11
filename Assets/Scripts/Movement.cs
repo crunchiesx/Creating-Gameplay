b1b7e9ef84037b8ad5972dcfe7da68f642140f5b
Justine Brylle VIllacarlos <134256171+crunchiesx@users.noreply.github.com>
2026-04-11 17:06:29 +0800

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private SpriteRenderer fireSprite;
+
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float turnSpeed = 10f;
+
+    private Rigidbody2D playerRb;
+
+    private float moveAxis;
+    private float turnAxis;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveAxis = value.Get<float>() * moveSpeed;
+
+        if (fireSprite != null)
+        {
+            fireSprite.color = new Color(fireSprite.color.r, fireSprite.color.g, fireSprite.color.b, value.Get<float>());
+        }
+    }
+
+    private void OnTurn(InputValue value)
+    {
+        turnAxis = value.Get<float>() * turnSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(transform.up * moveAxis);
+        playerRb.AddTorque(turnAxis);
+    }
+
+    private void OnDisable()
+    {
+        moveAxis = 0f;
+        turnAxis = 0f;
+        playerRb.linearVelocity = Vector2.zero;
+        playerRb.angularVelocity = 0f;
+
+        if (fireSprite != null)
+        {
+            fireSprite.color = new Color(fireSprite.color.r, fireSprite.color.g, fireSprite.color.b, 0f);
+        }
+    }
+}
