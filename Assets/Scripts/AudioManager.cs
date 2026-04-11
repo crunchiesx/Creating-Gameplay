b61d16d201b1afc1d2e380a157d59ec6541c8ee9
Justine Brylle VIllacarlos <134256171+crunchiesx@users.noreply.github.com>
2026-04-11 18:21:10 +0800

    Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySFX(AudioClip clip, float volumeScale = 1f)
+    {
+        audioSource.PlayOneShot(clip, volumeScale);
+    }
+}
