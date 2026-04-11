1571409375dc3f0ef253cce528140879feda3267
Justine Brylle VIllacarlos <134256171+crunchiesx@users.noreply.github.com>
2026-04-11 20:09:37 +0800

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public ObjectPool explosionPool;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+}
