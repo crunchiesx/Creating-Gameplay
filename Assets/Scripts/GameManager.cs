f28ab3ed024fc3b96388903fb4d4b4320d9e8b43
Justine Brylle VIllacarlos <134256171+crunchiesx@users.noreply.github.com>
2026-04-12 16:34:40 +0800

    Player UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [Header("References")]
+    [SerializeField] private PlayerUI[] playerUI;
+
     [Header("Object Pools")]
     public ObjectPool explosionPool;
     public ObjectPool[] asteroidPools;
@@ -44,6 +47,8 @@ public class GameManager : MonoBehaviour
 
     public void ReportPlayerDeath(GameObject player, int playerNumber, int lives)
     {
+        playerUI[playerNumber].UpdateLives(lives);
+
         if (lives > 0)
         {
             StartCoroutine(ReEnablePlayer(player));
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private Text livesDisplay;
+    [SerializeField] private Text scoreDisplay;
+
+    public void SetPlayerColor(Color playerColor)
+    {
+        livesDisplay.color = playerColor;
+        scoreDisplay.color = playerColor;
+    }
+
+    public void UpdateLives(int lives)
+    {
+        switch (lives)
+        {
+            case 3:
+                livesDisplay.text = "^ ^ ^";
+                break;
+            case 2:
+                livesDisplay.text = "^ ^";
+                break;
+            case 1:
+                livesDisplay.text = "^";
+                break;
+            default:
+                livesDisplay.text = "";
+                break;
+        }
+    }
+
+    public void UpdateScore(int score)
+    {
+        scoreDisplay.text = score.ToString();
+    }
+}
